eeb9a82bb1e0711c579b381b9222a8acf5592ade
Therese C Kjær <therese@getacademy.no>
2021-07-18 19:35:23 +0200

    update

--- view.js
+++ view.js
@@ -4,9 +4,9 @@ html += `<h1> Velkommen Til den Årlige Turneringen</h1><br><h4>ThereseBall- Edi
 <p> <h2> Dato: 31.07.2021 kl 13.00 </h2> <br>
 -  Det blir delt inn i 4 puljer på selve dagen. 1 og 2 plass går til semifinale <br> <br> 
 -  1. plassene møter hverandre i ett grupperspill og 2. plassene i ett gruppespill.<br> <br>
+-  Fra semi finalen går 1. og 2 plassen fra hvert gruppespill til GRAND-FINALE!<br> <br>
 -  De som ikke gikk videre til semi skriver navnet sitt på en lapp, <br>
    Therese trekker en og personen går direkte til GRAND-FINALE! <br> <br>
--  Fra semi finalen går 1. og 2 plassen fra hvert gruppespill til GRAND-FINALE!
 </p>
 
 <button onclick="show()"> Til Reglene! </button> 
@@ -17,39 +17,39 @@ document.getElementById('app').innerHTML = html
 function show(){
     let html = `<button onclick="view()">...Tilbake... </button><br> 
 
-    <p>GULT KORT!<br>
+    <p><h2>GULT KORT!</h2>
         2 gule kort = stå over en runde. <br>
-        4 gule kort i ett spill = ute av spillet ! BUGHU!!!!
+        4 gule kort i ett spill = ute av spillet ! BUHU !!!!
       </p>
-    <h3> FEILSLAG </h3>`;
+    <h2> FEILSLAG </h2>`;
     for( let i = 0; i < model.rules.length; i ++){
         if (model.rules[i].name === 'feilslag'){
             html += `
           ${showWrongHitsHtml(i)}`
         }
     }
-     html += '<h3> KROKKING </h3>';
+     html += '<h2> KROKKING </h2>';
     for( let i = 0; i < model.rules.length; i ++){
         if (model.rules[i].name === 'Krokking'){
             html += `
           ${showCrokkingHtml(i)}`
         }
     }
-    html += '<h3> PORT-REGLER </h3>';
+    html += '<h2> PORT-REGLER </h2>';
     for( let i = 0; i < model.rules.length; i ++){
         if (model.rules[i].name === 'Port-regler'){
             html += `
           ${showGateRulesHtml(i)}`
         }
     }
-    html += '<h3> FINALE-REGLER </h3>';
+    html += '<h2> FINALE-REGLER </h2>';
     for( let i = 0; i < model.rules.length; i ++){
         if (model.rules[i].name === 'finale-regler'){
             html += `
           ${showFinalRulesHtml(i)}`
         }
     }
-    html += '<h3> ØVRIGE-REGLER </h3>';
+    html += '<h2> ØVRIGE-REGLER </h2>';
     for( let i = 0; i < model.rules.length; i ++){
         if (model.rules[i].name === 'øvrige-regler'){
             html += `
@@ -62,8 +62,8 @@ function show(){
 function showWrongHitsHtml(index){
 html = ``
             html += `
-            <hr>Nummer : ${model.rules[index].number} <br>
-            <p>${model.rules[index].rule}</p>
+            <hr>Nr: ${model.rules[index].number} <br>
+            <p>Regel: <br>${model.rules[index].rule}</p>
             <tt> ${model.rules[index].consequences} </tt>`
         
         return html
@@ -72,16 +72,16 @@ html = ``
 function showCrokkingHtml(index){
     html = ``
                 html += `
-                <hr>Nummer : ${model.rules[index].number} <br>
-                <p>${model.rules[index].rule}</p>
+                <hr>Nr: ${model.rules[index].number} <br>
+                <p>Regel:<br> ${model.rules[index].rule}</p>
                 <tt> ${model.rules[index].consequences} </tt>`
             return html
     }
     function showGateRulesHtml(index){
         html = ``
                     html += `
-                    <hr>Nummer : ${model.rules[index].number} <br>
-                    <p>${model.rules[index].rule}</p>
+                    <hr>Nr: ${model.rules[index].number} <br>
+                    <p>Regel:<br> ${model.rules[index].rule}</p>
                     <tt> ${model.rules[index].consequences} </tt>`
                 return html
         }
@@ -89,16 +89,16 @@ function showCrokkingHtml(index){
     function showFinalRulesHtml(index){
         html = ``
                 html += `
-                <hr>Nummer : ${model.rules[index].number} <br>
-                <p>${model.rules[index].rule}</p>
+                <hr>Nr: ${model.rules[index].number} <br>
+                <p>Regel:<br> ${model.rules[index].rule}</p>
                 <tt> ${model.rules[index].consequences} </tt>`
         return html
     }
     function showGeneralRulesHtml(index){
         html = ``
                 html += `
-                <hr>Nummer : ${model.rules[index].number} <br>
-                <p>${model.rules[index].rule}</p>
+                <hr>Nr: ${model.rules[index].number} <br>
+                <p>Regel:<br> ${model.rules[index].rule}</p>
                 <tt> ${model.rules[index].consequences} </tt>`
         return html
     }
